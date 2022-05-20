231d322eca21e2c27cfd055ce5c0b3166b8bf454
Kefah Aied Albashityalshaer <kefahaied@gmail.com>
2022-05-21 02:00:06 +0300

    Time: 105 ms (72.80%), Space: 14.1 MB (68.55%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -7,10 +7,10 @@ class Solution:
         nums3.sort()
         median = None
         mid = None
-        mid = int(len(nums3)/2)
+        mid = len(nums3)//2
         
         if len(nums3) % 2 != 0:
             median = nums3[mid]
         else:
-            median = float((nums3[mid-1] + nums3[mid])/2.0)
+            median = (nums3[mid-1] + nums3[mid])/2.0
         return median
