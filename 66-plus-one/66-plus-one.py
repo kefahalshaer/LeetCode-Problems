ab5bd62437f21618b888ebf307177239bb96fd9a
Kefah Aied Albashityalshaer <kefahaied@gmail.com>
2022-05-27 22:47:39 +0300

    Time: 43 ms (55.59%), Space: 13.9 MB (11.25%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,11 +1,11 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        l1 = [str(i) for i in digits] #covert to str list
-        s1 = ''.join(l1)
-        n1 = int(s1) + 1
-        l3 = [i for i in str(n1)]
-        return [int(i) for i in l3]
+        # l1 = [str(i) for i in digits] #covert to str list
+        # s1 = ''.join(l1)
+        # n1 = int(s1) + 1
+        # l3 = [i for i in str(n1)]
+        # return [int(i) for i in l3]
     
-        
-        
-        
+        num = int(''.join(str(i) for i in digits))
+        num += 1
+        return [int(s) for s in list(str(num))]
