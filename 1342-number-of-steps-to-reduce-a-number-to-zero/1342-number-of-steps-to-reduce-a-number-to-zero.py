acb672f53e6f0c451f671693726fc4fff75f8dc8
Kefah Aied Albashityalshaer <kefahaied@gmail.com>
2022-05-27 19:33:49 +0300

    Time: 38 ms (64.36%), Space: 13.8 MB (53.50%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -4,10 +4,9 @@ class Solution:
         while (num != 0):
             if num % 2 == 0: # num is even
                 num /= 2
-                count += 1
             else:
                 num -= 1
-                count += 1
+            count += 1
         return count 
         
                     
