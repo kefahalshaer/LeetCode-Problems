33a0c232f5fd8ebf43cf1dc0b77e0bc1d3b540c7
Kefah Aied Albashityalshaer <kefahaied@gmail.com>
2022-06-04 20:01:31 +0300

    Time: 859 ms (7.79%), Space: 26 MB (29.91%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -3,19 +3,17 @@ class Solution:
         # 908 ms	26 MB
         # return len(nums) != len(set(nums))  
         
+        # 464 ms  25.9 MB
         # solve it by counting them in a hashmap
         hashMap = {}
         for num in nums:
             if num not in hashMap:
                 hashMap[num] = 1 
             else: return True
-        return False
-        
-        
-        
+        return False  
             
             
-            
+     # slower solution        
         #     hashMap[num] = hashMap.get(num, 0) + 1 # return 0 when the number is not found
         # for k,v in hashMap.items():
         #     if v > 1: # check dublicates
