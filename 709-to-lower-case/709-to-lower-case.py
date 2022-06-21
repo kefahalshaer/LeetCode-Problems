873f4818959807485a4d2e510b7dcffbd991dff0
Kefah Aied Albashityalshaer <kefahaied@gmail.com>
2022-06-22 00:15:29 +0300

    Time: 30 ms (92.73%), Space: 13.8 MB (94.33%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.py
+++ 709-to-lower-case/709-to-lower-case.py
@@ -1,3 +1,16 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.lower()
+        # return s.lower()
+        '''
+        ASCII value of uppercase alphabets – 65 to 90.
+        ASCII value of lowercase alphabets – 97 to 122.
+        '''
+    
+        out = ''
+        for c in s:
+            if c.isupper():
+                out+= chr(ord(c) + 32)
+
+            else: 
+                out+= c
+        return out
