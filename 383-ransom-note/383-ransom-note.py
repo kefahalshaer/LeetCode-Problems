5ba5ebabdbdaff0770a683b37181498ef359ed0f
Kefah Aied Albashityalshaer <kefahaied@gmail.com>
2022-07-11 21:12:50 +0300

    Time: 68 ms (78.07%), Space: 14.1 MB (94.00%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,15 +1,24 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ht1 = {}
-        ht2 = {}
-        for r in ransomNote:
-            ht1[r] = ht1.get(r, 0) + 1 
-        for m in magazine:
-            ht2[m] = ht2.get(m, 0) + 1
-        res = []
-        for k in ht1:
-            if k in ht2 and ht1[k] <= ht2[k]:
-                res.append(True)
-            else: res.append(False)
-        return all(res)
+        dm = Counter(magazine)
+        dr = Counter(ransomNote)
+        
+        for key , value in dr.items():
+            
+            if  value > dm[key]:
+                return False
+            
+        return True
+        # ht1 = {}
+        # ht2 = {}
+        # for r in ransomNote:
+        #     ht1[r] = ht1.get(r, 0) + 1 
+        # for m in magazine:
+        #     ht2[m] = ht2.get(m, 0) + 1
+        # res = []
+        # for k in ht1:
+        #     if k in ht2 and ht1[k] <= ht2[k]:
+        #         res.append(True)
+        #     else: res.append(False)
+        # return all(res)
         
