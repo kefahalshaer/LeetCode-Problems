821b14cd8e680391b4ba06b821eca80f7a63d43e
Kefah Aied Albashityalshaer <kefahaied@gmail.com>
2022-07-11 21:11:25 +0300

    Time: 132 ms (20.10%), Space: 14.2 MB (18.43%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ht1 = {}
+        ht2 = {}
+        for r in ransomNote:
+            ht1[r] = ht1.get(r, 0) + 1 
+        for m in magazine:
+            ht2[m] = ht2.get(m, 0) + 1
+        res = []
+        for k in ht1:
+            if k in ht2 and ht1[k] <= ht2[k]:
+                res.append(True)
+            else: res.append(False)
+        return all(res)
+        
